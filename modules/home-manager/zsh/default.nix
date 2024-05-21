24593f907e7875af4cf6e43c53719417c5b6e74e
lplawlor <lplawlor@protonmail.com>
2024-05-21 01:11:14 -0230

    ZSH history improvments + syntax highlighting

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -6,7 +6,28 @@ in {
   config = lib.mkIf cfg.enable {
     programs.zsh = {
       enable = true;
+      history = {
+        # Ignore duplicate history entries
+        ignoreDups = true;
+        ignoreAllDups = true;
+
+        # Don't save commands beginning with " " to the history
+        ignoreSpace = true;
+
+        # Share history between sessions
+        share = true;
+      };
+
+      # Only search through history entries matching the current partial command
+      historySubstringSearch.enable = true;
+
+      # Add Fish-style autosuggestions
       autosuggestion.enable = true;
+
+      # Enable syntax highlighting
+      syntaxHighlighting.enable = true;
+
+      # OMZ config
       oh-my-zsh = {
         enable = true;
         theme = "nicoulaj";
@@ -17,6 +38,8 @@ in {
           "zoxide"
         ];
       };
+
+      # Aliases
       shellAliases = {
         l = "lsd";
         ls = "lsd";
@@ -25,8 +48,12 @@ in {
         lla = "lsd -lA";
         c = "clear";
       };
+
       localVariables = {
+        # Use bat (without just line numbers) as the default pager
         PAGER = "bat -n";
+
+        # Use nvim as the default editor
         EDITOR = "nvim";
       };
     };
